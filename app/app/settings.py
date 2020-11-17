c9417cffefbb7d6482dcfc0043d6a213aa9f3cea
Victor Niño <ninomtz.victor@gmail.com>
2020-11-17 03:04:17 -0600

    bug cors fixed

--- app/app/settings.py
+++ app/app/settings.py
@@ -51,14 +51,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
@@ -159,13 +158,11 @@ SIMPLE_JWT = {
 }
 
 # Cors configuration
-CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000'
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000/',
+    'https://localhost:3000',
+    'http://127.0.0.1:3000/',
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
