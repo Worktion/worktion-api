d0ad4ab3b1b8db3b43d034cd771ee713b01f7052
Victor Niño <ninomtz.victor@gmail.com>
2020-11-26 22:57:11 -0600

    Models to routine deatil

--- app/routines/models.py
+++ app/routines/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from exercises.models import Exercise
 
 User = get_user_model()
 
@@ -35,3 +36,33 @@ class Routine(models.Model):
     dificulty = models.CharField(max_length=15, choices=DIFFICULTIES)
     muscle_group = models.CharField(max_length=10, choices=MUSCLE_GROUPS)
     cover = models.ImageField(upload_to='routine-images')
+
+
+class ExecutionBlock(models.Model):
+    """ Model to wrap the set of execution exercise """
+    routine = models.ForeignKey(
+        Routine,
+        related_name='blocks_routine',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=50)
+
+
+class ExecutionExercise(models.Model):
+    """ Model for the execution of a exercise """
+    TYPE_EXECUTION = (
+        ('secs', 'Seconds'),
+        ('reps', 'Repetitions')
+    )
+    execution_block = models.ForeignKey(
+        ExecutionBlock,
+        related_name='executions_block',
+        on_delete=models.CASCADE
+    )
+    exercise = models.ForeignKey(
+        Exercise,
+        related_name='execution_exercise',
+        on_delete=models.PROTECT
+    )
+    quantity = models.IntegerField()
+    typeExecution = models.CharField(max_length=11, choices=TYPE_EXECUTION)
